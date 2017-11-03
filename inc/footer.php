e763bb7816dd9b3aa5ce09f58d459705d2753c53
Kazi Rahamatullah <kazirahmat58@gmail.com>
2017-11-04 05:44:33 +0600

    Employee/Student Attendance System PHP

---
+++ inc/footer.php
@@ -0,0 +1,9 @@
+<div class="well">
+			<h3>
+				www. raHMat project.com
+				<span class="pull-right">Like us: www.facebook.com/md.k.rahmat</span>
+			</h3>
+		</div>
+	</div>
+</body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Employee/Student Attendance System PHP</title>
+	<link rel="stylesheet" href="inc/bootstrap.min.css">
+	<script type="text/javascript" src="inc/bootstrap.min.js"></script>
+	<script type="text/javascript" src="inc/jquery.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="well text-center">
+			<h2>Employee/Student Attendance System PHP</h2>
+		</div>
